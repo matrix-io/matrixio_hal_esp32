fc903f59414c8f12e84e3b92d0aae438faa30785
Andres Calderon <andres.calderon@admobilize.com>
2018-01-10 11:51:14 -0500

    mmm, just a little msg

--- make/deploy.mk
+++ make/deploy.mk
@@ -1,6 +1,13 @@
+ESPTOOLPY := voice_esptool
+ESPPORT := ttyS0
+ESPTOOLPY_SERIAL := $(ESPTOOLPY) --port $(ESPPORT) --baud $(ESPBAUD) --before $(CONFIG_ESPTOOLPY_BEFORE) --after $(CONFIG_ESPTOOLPY_AFTER)
+ESPTOOLPY_WRITE_FLASH=$(ESPTOOLPY_SERIAL) write_flash $(if $(CONFIG_ESPTOOLPY_COMPRESSED),-z,-u) $(ESPTOOL_WRITE_FLASH_OPTIONS)
+
+DEPLOY_CMD:=$(ESPTOOLPY_WRITE_FLASH) $(ESPTOOL_ALL_FLASH_ARGS)
 deploy:
 	scp build/bootloader/bootloader.bin \
 	$(APP_BIN) \
 	build/partitions_singleapp.bin \
 	$(RPI_HOST):/tmp
-
+	@echo "In the RASPBERRY PI"
+	@echo $(DEPLOY_CMD) | sed -e 's:'$(PWD)/xxx/'::g'
