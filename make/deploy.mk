7bdd331b9a6ed2a49a1bdcd441b7f567ab4e75f5
Andres Calderon <andres.calderon@admobilize.com>
2018-01-10 12:35:01 -0500

    improving messages

--- make/deploy.mk
+++ make/deploy.mk
@@ -4,12 +4,20 @@ ESPTOOLPY_SERIAL := $(ESPTOOLPY) --port $(ESPPORT) --baud $(ESPBAUD) --before $(
 ESPTOOLPY_WRITE_FLASH=$(ESPTOOLPY_SERIAL) write_flash $(if $(CONFIG_ESPTOOLPY_COMPRESSED),-z,-u) $(ESPTOOL_WRITE_FLASH_OPTIONS)
 
 DEPLOY_CMD:=$(ESPTOOLPY_WRITE_FLASH) $(ESPTOOL_ALL_FLASH_ARGS)
+
+REMOTE_CMD:=$(DEPLOY_CMD) | sed -e "s=bootloader/==g" | sed -e "s=$(PWD)==g" | sed -e "s=/build=/tmp=g"
+
 deploy:
+	@echo "********************"	
+	@echo "Copying files"
+	@echo "********************"	
 	scp build/bootloader/bootloader.bin \
 	$(APP_BIN) \
 	build/partitions_singleapp.bin \
 	$(RPI_HOST):/tmp
 	@echo "********************"	
 	@echo "To program the ESP32"
-	@echo ssh $(RPI_HOST) $(DEPLOY_CMD) | sed -e "s=bootloader/==g" | sed -e "s=$(PWD)==g" | sed -e "s=/build=/tmp=g"
+	@echo "********************"	
+	@echo ssh $(RPI_HOST) $(REMOTE_CMD)
+
  
