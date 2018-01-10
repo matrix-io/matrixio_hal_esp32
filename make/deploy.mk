9d72988a025dd8d637118062aba7857eb1792d02
Andres Calderon <andres.calderon@admobilize.com>
2018-01-10 15:07:51 -0500

    fix ESPTOOL var

--- make/deploy.mk
+++ make/deploy.mk
@@ -1,9 +1,9 @@
-ESPTOOLPY := voice_esptool
+ESPTOOL := voice_esptool
 ESPPORT := /dev/ttyS0
-ESPTOOLPY_SERIAL := $(ESPTOOLPY) --port $(ESPPORT) --baud $(ESPBAUD) --before $(CONFIG_ESPTOOLPY_BEFORE) --after $(CONFIG_ESPTOOLPY_AFTER)
-ESPTOOLPY_WRITE_FLASH=$(ESPTOOLPY_SERIAL) write_flash $(if $(CONFIG_ESPTOOLPY_COMPRESSED),-z,-u) $(ESPTOOL_WRITE_FLASH_OPTIONS)
+ESPTOOL_SERIAL := $(ESPTOOL) --port $(ESPPORT) --baud $(ESPBAUD) --before $(CONFIG_ESPTOOL_BEFORE) --after $(CONFIG_ESPTOOL_AFTER)
+ESPTOOL_WRITE_FLASH=$(ESPTOOL_SERIAL) write_flash $(if $(CONFIG_ESPTOOL_COMPRESSED),-z,-u) $(ESPTOOL_WRITE_FLASH_OPTIONS)
 
-DEPLOY_CMD:=$(ESPTOOLPY_WRITE_FLASH) $(ESPTOOL_ALL_FLASH_ARGS)
+DEPLOY_CMD:=$(ESPTOOL_WRITE_FLASH) $(ESPTOOL_ALL_FLASH_ARGS)
 
 REMOTE_CMD:=$(DEPLOY_CMD) | sed -e "s=bootloader/==g" | sed -e "s=$(PWD)==g" | sed -e "s=/build=/tmp=g"
 
@@ -15,6 +15,7 @@ deploy:
 	$(APP_BIN) \
 	build/partitions_singleapp.bin \
 	$(RPI_HOST):/tmp
+	@echo ""
 	@echo "********************"	
 	@echo "To program the ESP32"
 	@echo "********************"	
