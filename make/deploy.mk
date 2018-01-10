1c99c1cf40719946a703e214166df506d7ee77fa
Andres Calderon <andres.calderon@admobilize.com>
2018-01-10 16:02:50 -0500

    ESPTOOLPY_SRC fixed

--- make/deploy.mk
+++ make/deploy.mk
@@ -1,11 +1,14 @@
-ESPTOOL := voice_esptool
+MV_ESPTOOLPY_SRC := voice_esptool
+
 ESPPORT := /dev/ttyS0
-ESPTOOL_SERIAL := $(ESPTOOL) --port $(ESPPORT) --baud $(ESPBAUD) --before $(CONFIG_ESPTOOL_BEFORE) --after $(CONFIG_ESPTOOL_AFTER)
-ESPTOOL_WRITE_FLASH=$(ESPTOOL_SERIAL) write_flash $(if $(CONFIG_ESPTOOL_COMPRESSED),-z,-u) $(ESPTOOL_WRITE_FLASH_OPTIONS)
 
-DEPLOY_CMD:=$(ESPTOOL_WRITE_FLASH) $(ESPTOOL_ALL_FLASH_ARGS)
+MV_ESPTOOLPY := $(MV_ESPTOOLPY_SRC) --chip esp32
 
-REMOTE_CMD:=$(DEPLOY_CMD) | sed -e "s=bootloader/==g" | sed -e "s=$(PWD)==g" | sed -e "s=/build=/tmp=g"
+MV_ESPTOOLPY_SERIAL := $(MV_ESPTOOLPY) --port $(ESPPORT) --baud $(ESPBAUD) --before $(CONFIG_ESPTOOLPY_BEFORE) --after $(CONFIG_ESPTOOLPY_AFTER)
+
+ESPTOOL_WRITE_FLASH=$(MV_ESPTOOLPY_SERIAL) write_flash $(if $(CONFIG_ESPTOOL_COMPRESSED),-z,-u) $(ESPTOOL_WRITE_FLASH_OPTIONS)
+
+DEPLOY_CMD:=$(ESPTOOL_WRITE_FLASH) $(ESPTOOL_ALL_FLASH_ARGS)  | sed -e "s=bootloader/==g" | sed -e "s=$(PWD)==g" | sed -e "s=/build=/tmp=g"
 
 deploy:
 	@echo "********************"	
@@ -19,6 +22,6 @@ deploy:
 	@echo "********************"	
 	@echo "To program the ESP32"
 	@echo "********************"	
-	@echo ssh $(RPI_HOST) $(REMOTE_CMD)
+	@echo ssh $(RPI_HOST) $(DEPLOY_CMD)
 
  
